3de2bdc0e8131807b70ec3d22e48792d8c2649bf
JesseSoldat <jsoldat@hotmail.com>
2017-09-23 12:53:19 -0400

    recipe reducer is set up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromRecipe from '../store/recipe.reducer';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipeState: Observable<fromRecipe.State>;
 
-  constructor() { }
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private store: Store<fromRecipe.FeatureState>) { }
 
   ngOnInit() {
+    this.recipeState = this.store.select('recipes');
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { RecipesComponent } from './recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipeItemComponent } from 
+'./recipe-list/recipe-item/recipe-item.component';
+import { StoreModule } from '@ngrx/store';
+import { recipeReducer } from './store/recipe.reducer';
 
 @NgModule({
   declarations: [
@@ -14,12 +18,15 @@ import { RecipesRoutingModule } from './recipes-routing.module';
     RecipeDetailComponent,
     RecipeEditComponent,
     RecipeListComponent,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeItemComponent
   ],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
+    SharedModule,
+    StoreModule.forFeature('recipes', recipeReducer),
     RecipesRoutingModule,
+
   ]
 })
 export class RecipesModule {}
