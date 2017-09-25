7a62be68695a68b30c3e785dbc759a1f84b8ab69
JesseSoldat <jsoldat@hotmail.com>
2017-09-25 10:53:33 -0400

    details works

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/take';
+
+import * as fromRecipe from '../store/recipe.reducer';
+import * as RecipeActions from '../store/recipe.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,10 +13,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  recipeState: Observable<fromRecipe.State>;
+  id: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private store: Store<fromRecipe.FeatureState>) { }
 
   ngOnInit() {
+    this.route.params 
+      .subscribe((params: Params) => {
+        this.id = +params['id'];
+        this.recipeState = this.store.select('recipes');
+      });
+  }
+
+  onAddToShoppingList() {
+    
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+
+  onDeleteRecipe() {
+    this.store.dispatch(new RecipeActions.DeleteRecipe(this.id));
+    this.router.navigate(['/recipes']);
   }
 
 }
